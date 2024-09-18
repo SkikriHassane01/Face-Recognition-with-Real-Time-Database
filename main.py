e8ffe9939b1e5a349209ac68237b3e871256f04d
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-18 18:57:06 +0100

    Run our webcam

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 450)
+cap.set(4, 415)
+
+while True:
+    success, img = cap.read()
+    if not success:
+        print('Failed to read frame from the camera')
+        break
+    
+    cv2.imshow("Webcame", img)
+    cv2.flip(img,1) # horizontally
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
